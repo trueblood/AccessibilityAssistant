5f9034e0373e99ffc19723dbd2f23c6dd7a34bf5
Tim <timtrueblood@outlook.com>
2023-01-22 10:57:28 -0500

    import cleanup

--- src/machinelearning.py
+++ src/machinelearning.py
@@ -2,9 +2,6 @@ from datasets import load_dataset
 from txtai.pipeline import HFTrainer
 import pandas as pd
 from transformers import pipeline
-from transformers import PegasusForConditionalGeneration
-from transformers import PegasusTokenizer
-from transformers import pipeline
 
 
 class MachineLearning:
@@ -37,58 +34,3 @@ class MachineLearning:
         for x in result:
             value.append(x['summary_text'])
         return value
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    def summarize_text(text):
-
-        # Pick model
-        model_name = "google/pegasus-xsum"
-
-        # Load pretrained tokenizer
-        pegasus_tokenizer = PegasusTokenizer.from_pretrained(model_name)
-
-        example_text = str(text)
-        example_text = """ and/or other fora provided by copyleft.org may not necessarily reflect the views of the contributors' employers and/or organizations sponsoring the project and/or organizations republishing copyleft.org's materials. Generally speaking, unless stated otherwise, please assume that individuals contribute to copyleft.org in their personal capacity.  """
-
-
-        # Define PEGASUS model
-        pegasus_model = PegasusForConditionalGeneration.from_pretrained(model_name)
-
-        # Create tokens
-        tokens = pegasus_tokenizer(example_text, truncation=True, padding="longest", return_tensors="pt")
-
-
-        # Summarize text
-        encoded_summary = pegasus_model.generate(**tokens)
-
-        # Decode summarized text
-        decoded_summary = pegasus_tokenizer.decode(
-           encoded_summary[0],
-             skip_special_tokens=True
-        )
-
-        # Define summarization pipeline 
-        summarizer = pipeline(
-            "summarization", 
-            model=model_name, 
-            tokenizer=pegasus_tokenizer, 
-        )
-
-       # result = summarizer(str(text))
-
-        summary = summarizer(example_text, min_length=0, max_length=len(text))
-
-        return summary[0]["summary_text"]
-        #return "tesst"
